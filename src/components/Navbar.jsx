e8fd9c14b22f7e093f1fc1a0411e04475bbc5339
vinz-DY <vincentducamp259@hotmail.com>
2023-12-10 20:31:57 +0100

    navbar ok avec burger menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Logo from "../assets/logo3.jpg";
-import { GiHamburgerMenu } from "react-icons/gi";
+import { GiHamburgerMenu} from "react-icons/gi";
+import { FaTimes} from "react-icons/fa";
 
 const Navbar = () => {
     const [nav, setNav] = useState(false)
@@ -27,10 +28,16 @@ const Navbar = () => {
 
       {/* Burger Menu */}
       <div onClick={handleclick} className="md:hidden z-10">
-        <GiHamburgerMenu />
+        {!nav ? <GiHamburgerMenu /> : <FaTimes />}
       </div>
       {/* Mobile menu */}
-      <ul className={!nav ? 'hidden' : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"}>
+      <ul
+        className={
+          !nav
+            ? "hidden"
+            : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"
+        }
+      >
         <li className="py-6 text-4xl">Home</li>
         <li className="py-6 text-4xl">About</li>
         <li className="py-6 text-4xl">Skills</li>
