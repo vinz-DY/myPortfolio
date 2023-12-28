2748af65b3854d6b164252ec172f78064b500bde
vinz-DY <vincentducamp259@hotmail.com>
2023-12-28 21:57:48 +0100

    projets ok

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -78,30 +78,6 @@ const Work = () => {
           </div>
           {/*grid item*/}
           <div
-            style={{ backgroundImage: `url(${Just})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
-          >
-            {/* Hover Effects */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">
-                Vanilla JS Application
-              </span>
-              <div className="pt-8 text-center">
-                <a href="https://vinz-dy.github.io/Just_React/" target="blank">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white hover:bg-[#cbbeff] hover:text-white hover:animate-pulse text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a href="https://github.com/vinz-DY/Just_React" target="blank">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white hover:bg-[#cbbeff] hover:text-white  text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-          {/*grid item*/}
-          <div
             style={{ backgroundImage: `url(${Collec})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
@@ -132,14 +108,40 @@ const Work = () => {
           </div>
           {/*grid item*/}
           <div
-            style={{ backgroundImage: `url(${Portfolio})` }}
+            style={{ backgroundImage: `url(${Just})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
             {/* Hover Effects */}
             <div className="opacity-0 group-hover:opacity-100">
               <span className="text-2xl font-bold text-white tracking-wider">
+                Vanilla JS Application
+              </span>
+              <div className="pt-8 text-center">
+                <a href="https://vinz-dy.github.io/Just_React/" target="blank">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white hover:bg-[#cbbeff] hover:text-white hover:animate-pulse text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="https://github.com/vinz-DY/Just_React" target="blank">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white hover:bg-[#cbbeff] hover:text-white  text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          {/*grid item*/}
+          <div
+            style={{ backgroundImage: `url(${Portfolio})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100 flex flex-col">
+              <span className="text-2xl font-bold text-white tracking-wider">
                 React JS Application
               </span>
+              <span className="text-center font-bold">Tailwind CSS</span>
               <div className="pt-8 text-center">
                 <a href="https://github.com/vinz-DY/myPortfolio" target="blank">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white hover:bg-[#cbbeff] hover:text-white  text-gray-700 font-bold text-lg">
